fb4b12977a1c863a0171de7181196482e027dd97
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2024-02-25 20:34:11 +0400

    change text

--- components/vans/VanHero.tsx
+++ components/vans/VanHero.tsx
@@ -15,17 +15,20 @@ const VanHero = () => {
     <div className="2xl:mx-44 xl:mx-32 md:mx-16">
       <div className="flex flex-col flex-1 pt-10 p-6 items-center justify-center">
         <h1 className="text-xl sm:text-4xl text-center">
-          Transform your journey with our revolutionary van rental experience!
+          Effortlessly find, book, and ride in our magnificent vans.
+        </h1>
+        {/* Transform your journey with our revolutionary van rental experience!
           Dive into a captivating adventure where every moment is filled with
           excitement and ease. This isn't just a rental, it's your gateway to
-          unforgettable memories.
-        </h1>
+          unforgettable memories. */}
 
         <p className="mt-4 text-sm text-center">
-          Immerse yourself in the ultimate van rental experience and savor the
-          journey with our exclusive special offer. Book now and relish the
-          added perks that make your adventure truly exceptional.
+          Streamline your rental experience with our simple and efficient
+          process.
         </p>
+        {/* Immerse yourself in the ultimate van rental experience and savor the
+          journey with our exclusive special offer. Book now and relish the
+          added perks that make your adventure truly exceptional. */}
 
         <CustomButton
           title="Explore Vans"
