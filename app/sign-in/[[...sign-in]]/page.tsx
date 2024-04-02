c5676f3c128c121329375a883325ed6493ee22b4
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2024-04-02 19:06:48 +0400

    styling

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center mt-10">
       <SignIn />
     </div>
   );
